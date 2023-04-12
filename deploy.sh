b4eb30a8f7d0c8a6707e7c5f056d70977cb1b7a6
Anatolii Sakhnik <sakhnik@gmail.com>
2023-04-12 11:37:19 +0300

    Bundle eruda for local run

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,10 @@ mkdir -p qr
 (
 cd qr
 rsync -raP --delete ${this_dir}/src/* . --exclude=html5-qrcode.js
-wget -c https://unpkg.com/html5-qrcode -O html5-qrcode.js
+wget https://unpkg.com/html5-qrcode -O html5-qrcode.js
 sed -i 's|https://unpkg.com/html5-qrcode|html5-qrcode.js|' index.html
+wget https://cdn.jsdelivr.net/npm/eruda -O eruda.js
+sed -i 's|https://cdn.jsdelivr.net/npm/eruda|eruda.js|' index.html
 zip -r ../qr.zip *
 )
 rsync -raP --delete --chown 33:33 qr.zip sakhnik.com:/var/www/sakhnik.com/
